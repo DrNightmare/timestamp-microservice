1d89153a23e5b93edd5cc759fa2913678a143ca7
Arvind <arvind687@gmail.com>
2016-12-12 16:06:39 +0530

    Initial commit

---
+++ timeStampMS.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var moment = require('moment');
+var path = require('path');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:timeStamp', function(req, res) {
+	var timeStamp = req.params.timeStamp;
+	var unixTime, naturalTime;
+	if (moment.unix(Number(timeStamp)).isValid()){
+		unixTime = Number(timeStamp);
+		naturalTime = moment.unix(Number(timeStamp)).format("MMMM D, YYYY");
+	}
+	else if (moment(timeStamp, "MMMM D, YYYY", true).isValid()) {
+		unixTime = Number(moment(timeStamp, "MMMM D, YYYY").format("X"));
+		naturalTime = timeStamp;
+	}
+	else {
+		unixTime = naturalTime = null;
+	}
+	res.send({"unix" : unixTime, "natural" : naturalTime});
+});
+
+app.listen('3000', function(err, data) {
+	if (err) console.log(err);
+})
